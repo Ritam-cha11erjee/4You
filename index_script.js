3bf41795efe4af56879c59cbebc358ba0b1740d0
Ritam Chatterjee <chatterjeeritam50@gmail.com>
2024-11-17 12:45:21 +0530

    first commit

---
+++ index_script.js
@@ -0,0 +1,46 @@
+ let slideshow = document.querySelector(".slideshow");
+ let element = document.getElementsByClassName("SSimage");
+ let prev = document.getElementById("prev");
+ let next = document.getElementById("next");
+let counter = 0;
+let maxSlides = 4;
+let intervalID;
+
+initiateSlideshow();
+
+function initiateSlideshow(){
+
+    intervalID = setInterval(() => {
+        goNext();
+    }, 5000);
+}
+
+ next.addEventListener("click", () => {intervalID = clearInterval(intervalID); goNext();})
+
+ prev.addEventListener("click", goPrev)
+
+
+ function goNext(){
+
+    if(counter == maxSlides-1){
+        counter = 0;
+    }
+    else
+    counter ++;
+    slideshow.style.transform = `translateX(-${counter*25}%)`
+ }
+
+ function goPrev(){
+
+    intervalID = clearInterval(intervalID);
+
+
+    if(counter == 0){
+        counter = maxSlides-1;
+    }
+    else
+    counter --;
+    slideshow.style.transform = `translateX(-${counter*25}%)`
+
+ }
+ 
